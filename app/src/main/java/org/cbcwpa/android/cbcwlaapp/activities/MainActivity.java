fd9bd1b6319dc0914e9f6fe633dc1ce5f580bc2e
Shinyi Chen <jchen@isi.edu>
2017-05-25 18:05:32 -0700

    MainActivity UI update

--- app/src/main/java/org/cbcwpa/android/cbcwlaapp/activities/MainActivity.java
+++ app/src/main/java/org/cbcwpa/android/cbcwlaapp/activities/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import org.cbcwpa.android.cbcwlaapp.R;
